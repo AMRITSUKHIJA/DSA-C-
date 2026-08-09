e6b3d067af9987eaf0eb2d291a713a4d33272fc1
Amrit Sukhija <amritsukhija26@gmail.com>
2026-08-09 23:21:23 +0530

    Time: 68 ms (5.32%), Space: 115.5 MB (24.47%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,73 +1,34 @@
-// class Solution {
-// public:
-//     vector<int> validSequence(string word1, string word2) {
-//         int n = word1.size();
-//         int m = word2.size();
-//         vector<int> ans(m);
-//         // last[j] = position in word1 that can be used
-//         // for word2[j] when matching from the right.
-//         vector<int> last(m, -1);
-//         int i = n - 1;
-//         int j = m - 1;
-//         // Match word2 from right to left.
-//         while (i >= 0 && j >= 0) {
-//             if (word1[i] == word2[j]) {
-//                 last[j] = i;
-//                 j--;
-//             }
-//             i--;
-//         }
-//         // Now greedily build the lexicographically smallest answer.
-//         bool usedMismatch = false;
-//         j = 0;
-//         for (i = 0; i < n && j < m; i++) {
-//             // Exact match: always the best choice.
-//             if (word1[i] == word2[j]) {
-//                 ans[j] = i;
-//                 j++;
-//             }
-//             // Use our one allowed mismatch.
-//             else if (!usedMismatch &&
-//                      (j == m - 1 || i < last[j + 1])) {
-
-//                 ans[j] = i;
-//                 j++;
-//                 usedMismatch = true;
-//             }
-//         }
-//         // Couldn't construct a sequence of length m.
-//         if (j != m) {
-//             return {};
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
-    vector<int> validSequence(string s,string t) {
-        int n=s.size(),m=t.size();
-        vector<int> dp(n+1),ans;
-        int j=m-1;
-        bool ok=1;
-        for(int i=n-1;i>=0;i--) {
-            dp[i]=dp[i+1];
-            if(j>=0&&s[i]==t[j]) {
+    vector<int> validSequence(string word1, string word2) {
+        //We will do a pre computation that in word1 how many chnaracters of word2 are matching thill that index
+        vector<int> countMatchingTillIndex((word1.size()+1), 0);
+        int i = word1.size()-1;
+        int j = word2.size()-1;
+        int countFound = 0;
+        while(i >= 0){
+            if(j >= 0 && word1[i] == word2[j]){
+                countFound++;
                 j--;
-                dp[i]++;
             }
+            countMatchingTillIndex[i] = countFound;
+            i--;
         }
-        j=0;
-        for(int i=0;i<n&&j<m;i++) {
-            if(s[i]==t[j]) {
+        i = 0;
+        j = 0;
+        bool canChange = true;
+        vector<int> ans;
+        while(i < word1.size() && j < word2.size()){
+            if((word1[i] == word2[j]) || (canChange && countMatchingTillIndex[i+1] >= (word2.size()-1-j))){
                 ans.push_back(i);
+                if(word1[i] != word2[j]) canChange = false;
                 j++;
-            }
-            else if(ok&&dp[i+1]>=m-j-1) {
-                ans.push_back(i);
-                ok=0;
-                j++;
-            }
+            }   
+            i++;
         }
-        return ans.size()==m?ans:vector<int>{};
+        if(j < word2.size()){
+            ans.clear();
+        }
+        return ans;
     }
 };
