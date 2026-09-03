3eba2792961948e4b8317d2ca30b448198ea95a6
Amrit Sukhija <amritsukhija26@gmail.com>
2026-09-02 23:49:48 -0700

    Time: 3 ms (72.51%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        // Find the smallest odd number
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                minOdd = min(minOdd, x);
+            }
+        }
+        // If there are no odd numbers,
+        // everything is already even.
+        if (minOdd == INT_MAX) {
+            return true;
+        }
+        // Any even number smaller than the smallest odd
+        // cannot be converted to odd.
+        for (int x : nums1) {
+            if (x % 2 == 0 && x < minOdd) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
